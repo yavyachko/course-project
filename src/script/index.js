d3403214bc389a0f44e3dfc0046ff1559e8acd20
yavyachko <133366660+yavyachko@users.noreply.github.com>
2023-12-10 20:37:23 +0300

    Add files via upload

--- src/script/index.js
+++ src/script/index.js
@@ -3,7 +3,7 @@ window.onscroll = function () {
     let scrolled = window.pageYOffset || document.documentElement.scrollTop;
     let posTop = window.pageYOffset;
     if (scrolled !== 0) {
-        document.querySelector('header').style.background = 'rgba(28,28,40,' + posTop / 100 + ')';
+        document.querySelector('header').style.background = 'rgba(0,0,0,' + posTop / 100 + ')';
     } else {
         document.querySelector('header').style.background = 'rgba(28,28,40, 0)';
     }
@@ -84,15 +84,15 @@ const mobileMenu = () => {
                 elementImg[0].style.visibility = 'visible';
                 elementImg[0].style.top = timePassed + 'px';
                 document.body.style.overflow = 'hidden';
-                console.log(document.location.pathname);
-                document.location.pathname == '/course-project/' ? document.getElementsByClassName('burgerHeaderImg')[0].src = '/course-project/src/img/icons/fi_x.svg' : document.getElementsByClassName('burgerHeaderImg')[0].src = '../src/img/icons/fi_x.svg';
+                document.location.pathname == '/' ? document.getElementsByClassName('burgerHeaderImg')[0].src = './src/img/icons/fi_x.svg' : document.getElementsByClassName('burgerHeaderImg')[0].src = '../src/img/icons/fi_x.svg';
+                document.location.href.substring(addresExecution(document.location.href), document.location.href.length) == '404.html' ? document.getElementsByClassName('burgerHeaderImg')[0].src = './src/img/icons/fi_x.svg' : document.getElementsByClassName('burgerHeaderImg')[0].src = '../src/img/icons/fi_x.svg';
                 if (timePassed > header.offsetHeight - 8) clearInterval(timer);
             }
             else if (!flag) {
                 header.style.background = 'rgba(28,28,40, 1)';
                 elementImg[0].style.visibility = 'hidden';
-                console.log(document.location.pathname);
-                document.location.pathname == '/course-project/' ? document.getElementsByClassName('burgerHeaderImg')[0].src = '/course-project/src/img/icons/fi_menu.svg' : document.getElementsByClassName('burgerHeaderImg')[0].src = '../src/img/icons/fi_menu.svg';
+                document.location.pathname == '/' ? document.getElementsByClassName('burgerHeaderImg')[0].src = './src/img/icons/fi_menu.svg' : document.getElementsByClassName('burgerHeaderImg')[0].src = '../src/img/icons/fi_menu.svg';
+                document.location.href.substring(addresExecution(document.location.href), document.location.href.length) == '404.html' ? document.getElementsByClassName('burgerHeaderImg')[0].src = './src/img/icons/fi_menu.svg' : document.getElementsByClassName('burgerHeaderImg')[0].src = '../src/img/icons/fi_menu.svg';
                 elementImg[0].style.top = -(timePassed / 2) + 'px';
                 document.body.style.overflowY = 'scroll';
                 if (timePassed > header.offsetHeight) {
@@ -147,3 +147,91 @@ try {
 } catch (e) {
     console.log("No modals on this page")
 }
+//------------------
+
+const sliderContainer = document.querySelector('.communitySlider');
+const sliderTrack = document.querySelector('.sliderTrack');
+const sliderItems = document.querySelectorAll('.sliderItem');
+
+let currentSlide = 0;
+let isDragging = false;
+let startPos = 0;
+let currentTranslate = 0;
+let prevTranslate = 0;
+let animationID = 0;
+
+sliderItems.forEach((item, index) => {
+  // обработчик
+  item.addEventListener('mousedown', dragStart);
+  item.addEventListener('touchstart', dragStart);
+  item.addEventListener('mouseup', dragEnd);
+  item.addEventListener('touchend', dragEnd);
+  item.addEventListener('mouseleave', dragEnd);
+  item.addEventListener('mousemove', drag);
+  item.addEventListener('touchmove', drag);
+});
+
+function dragStart(e) {
+  // начальные координаты
+  if (e.type === 'touchstart') {
+    startPos = e.touches[0].clientX;
+  } else {
+    startPos = e.clientX;
+  }
+  isDragging = true;
+  // остановка анимации
+  cancelAnimationFrame(animationID);
+}
+
+function drag(e) {
+  if (isDragging) {
+    let currentPosition = 0;
+    if (e.type === 'touchmove') {
+      currentPosition = e.touches[0].clientX;
+    } else {
+      currentPosition = e.clientX;
+    }
+    // шаг слайдера по x
+    currentTranslate = prevTranslate + currentPosition - startPos;
+    // смещаем слайды
+    sliderTrack.style.transform = `translateX(${currentTranslate}px)`;
+  }
+}
+
+function dragEnd() {
+  // примняем згачения 
+  prevTranslate = currentTranslate;
+  isDragging = false;
+  // пуск анимации
+  animationID = requestAnimationFrame(settle);
+}
+
+function settle() {
+  // переключение на элемент
+  const slideWidth = sliderItems[0].offsetWidth;
+  const threshold = slideWidth / 4;
+  if (currentTranslate > threshold && currentSlide > 0) {
+    currentSlide--;
+  } else if (currentTranslate < -threshold && currentSlide < sliderItems.length - 1) {
+    currentSlide++;
+  }
+  currentTranslate = currentSlide * -slideWidth;
+  // смещение окончательное
+  sliderTrack.style.transform = `translateX(${currentTranslate}px)`;
+  // запуск
+  prevTranslate = currentTranslate;
+  animationID = requestAnimationFrame(settle);
+}
+
+function nextSlide() {
+  currentSlide++;
+  if (currentSlide === sliderItems.length) {
+    currentSlide = 0;
+  }
+  currentTranslate = currentSlide * -sliderItems[0].offsetWidth;
+  sliderTrack.style.transform = `translateX(${currentTranslate}px)`;
+  prevTranslate = currentTranslate;
+}
+
+const nextButton = document.getElementById('nextButton');
+nextButton.addEventListener('click', nextSlide);
